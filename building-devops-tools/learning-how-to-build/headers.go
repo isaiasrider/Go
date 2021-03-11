4b66696aef68ca85db09e5da6f92cd1e6ee6e425
isaias <isaias@MacBook-Pro-de-isaias.local>
2021-03-11 00:17:19 -0300

    aprendendo um jeito facinho de setar o header auth

--- building-devops-tools/learning-how-to-build/headers.go
+++ building-devops-tools/learning-how-to-build/headers.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"bytes"
-	"encoding/base64"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -14,16 +12,7 @@ func main(){
 	if err != nil {
 		log.Fatalln("nao foi possivel se autenticar")
 	}
-	buffer := &bytes.Buffer{}
-	enc := base64.NewEncoder(base64.URLEncoding, buffer)
-    enc.Write([]byte("user:passw0rd"))
-	enc.Close()
-	encondedCreds, err := buffer.ReadString('\n')
-	if err != nil && err.Error() != "EOF" {
-		log.Fatalln("failed to read encoded buffer")
-	}
-
-	req.Header.Add("Authorization", fmt.Sprintf("Basic %s",encondedCreds))
+    req.SetBasicAuth("user","passw0rd")
 	res, err := http.DefaultClient.Do(req)
 	defer res.Body.Close()
 	content, err := ioutil.ReadAll(res.Body)
