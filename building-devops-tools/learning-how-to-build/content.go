8e4aa864bd98286736c2eeed056ca3aa5401dcd6
isaias <isaias@MBP-de-isaias.meuintelbras.local>
2021-03-11 21:39:09 -0300

    analisando e lendo a anatomia de uma request

--- building-devops-tools/learning-how-to-build/content.go
+++ building-devops-tools/learning-how-to-build/content.go
@@ -1,12 +1,25 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 )
+type GetResponse struct {
+	ORIGIN string `json:"origin"`
+	URL string     `json:"url"`
+	HEADERS map[string]string `json:"headers"`
+}
 
+func (r *GetResponse) ToString() string {
+	s:= fmt.Sprintf("THIS IS WHAT WE HAVE ON OUR REQUEST\n Origin IP: %s\nURL: %s", r.ORIGIN, r.URL)
+	for k, v := range r.HEADERS {
+		s += fmt.Sprintf("Headers: %s = %s\n", k,v)
+	}
+	return s
+}
 func main() {
 	res, err := http.Get("http://httpbin.org/get")
 	if err != nil {
@@ -18,5 +31,11 @@ func main() {
 		log.Fatalln("Unable to read the body")
 	}
 
-	fmt.Print(string(content))
+	respContent := GetResponse{}
+	json.Unmarshal(content, &respContent)
+    fmt.Println(respContent.ToString())
 }
+
+
+
+
--- building-devops-tools/learning-how-to-build/headers.go
+++ building-devops-tools/learning-how-to-build/headers.go
@@ -8,16 +8,17 @@ import (
 )
 
 func main(){
-	req, err := http.NewRequest("GET", "https://httpbin.org/basic-auth/user/passw0rd", nil)
+	req, err := http.NewRequest("GET", "https://httpbin.org/basic-auth/user/p@ssword", nil)
 	if err != nil {
-		log.Fatalln("nao foi possivel se autenticar")
+		log.Fatalln("não foi possível guardar a request")
 	}
-    req.SetBasicAuth("user","passw0rd")
+    req.SetBasicAuth("user","p@ssword")
+
 	res, err := http.DefaultClient.Do(req)
 	defer res.Body.Close()
 	content, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-		log.Fatalln("nao foi possivel lero body")
+		log.Fatalln("nao foi possivel ler o body")
 	}
 
 	fmt.Println(string(content))
